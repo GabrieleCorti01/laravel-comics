1c1a109615ee4147e2032946066bee35df99e36e
Gabrielecorti01 <gcorti.info@gmail.com>
2021-10-25 23:36:45 +0200

    Si

---
+++ laravel-comics/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('title', 'Home')
+
+@section('contenuto')
+    <div class="container">
+        <h1>Ciao Ragazzi</h1>
+    </div>
+@endsection
---
+++ laravel-comics/resources/views/partials/header.blade.php
@@ -0,0 +1,39 @@
+    
+<header>
+
+    <nav>
+        <ul>
+            <li>
+                <a href="{{ route('characters') }}">CHARACTERS</a>
+            </li>
+            <li>
+                <a href="{{ route('comics') }}">COMICS</a>
+            </li>
+            <li>
+                <a href="{{ route('movies') }}">MOVIES</a>
+            </li>
+            <li>
+                TV
+            </li>
+            <li>
+                GAMES
+            </li>
+            <li>
+                COLLECTIONS
+            </li>
+            <li>
+                VIDEOS
+            </li>
+            <li>
+                FANS
+            </li>
+            <li>
+                NEWS
+            </li>
+            <li>
+                SHOP
+            </li>
+        </ul>
+    </nav>
+
+</header>
