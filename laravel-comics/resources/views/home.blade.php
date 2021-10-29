093ecff195dcf1d89553c9fcbfb3f71a0f971fa7
Gabrielecorti01 <gcorti.info@gmail.com>
2021-10-29 19:48:14 +0200

    Sezione Home terminata

--- laravel-comics/resources/views/home.blade.php
+++ laravel-comics/resources/views/home.blade.php
@@ -3,24 +3,33 @@
 @section('title', 'Home')
 
 @section('contenuto')
-<div id="series">
-        <div class="main-container">
-            
 
-            @foreach($series_list as $serie)
-        
-            <div class="card">
-                <a href="{{ route('serie', ['id' => $loop ->index]) }}">
-                <img src="{{ $serie['thumb'] }}" alt="">
-                <p>{{ $serie['title'] }}</p>
-                </a>
-            </div>
-        
+<!-- inizio sezione main series -->
+<section id="series">
 
-            @endforeach
-
-        </div>
-
-        
+    <!-- inizio sezione etichetta current series -->
+    <a href="#">
+    <div class="current-series">
+        <h2>CURRENT SERIES</h2>
     </div>
+    </a>
+
+    <div class="main-container">
+        
+        @foreach($series_list as $serie)
+    
+        <div class="card">
+            <a href="{{ route('serie', ['id' => $loop ->index]) }}">
+
+            <img src="{{ $serie['thumb'] }}" alt="">
+            <p>{{ $serie['title'] }}</p>
+
+            </a>
+        </div>
+    
+        @endforeach
+
+    </div>
+
+</section>
 @endsection
